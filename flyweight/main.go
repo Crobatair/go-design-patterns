3c7be4f3000c7b97f5f9cff83743dd37af5cbc10
Alejandro <jose5m45@gmail.com>
2025-01-08 23:01:01 -0600

    feat: add permissions example with Flyweight design pattern

--- flyweight/main.go
+++ flyweight/main.go
@@ -1,37 +1,41 @@
 package main
 
+import "fmt"
+
 type Permission struct {
 	Name string
 }
 
 type PermissionFlyweightFactory struct {
-	permissions map[string]*Permission
+	permissions map[int8]*Permission
 }
 
 func NewPermissionFlyweightFactory(systemPermissions []string) *PermissionFlyweightFactory {
 	factory := PermissionFlyweightFactory{
-		permissions: make(map[string]*Permission),
+		permissions: make(map[int8]*Permission),
 	}
 
-	for _, permission := range systemPermissions {
-		factory.permissions[permission] = &Permission{permission}
+	for i, permission := range systemPermissions {
+		factory.permissions[int8(i)] = &Permission{permission}
 	}
 
 	return &factory
 }
 
 // GetPermission returns a permission object if it exists in the factory, otherwise it returns nil
-func (p *PermissionFlyweightFactory) GetPermission(permissionName string) *Permission {
-	if _, ok := p.permissions[permissionName]; ok {
-		return p.permissions[permissionName]
+func (p *PermissionFlyweightFactory) GetPermission(permissionName string) int8 {
+	for idx, permission := range p.permissions {
+		if permission.Name == permissionName {
+			return idx
+		}
 	}
 
-	return nil
+	return -1
 }
 
 type User struct {
 	Name        string
-	Permissions []*Permission
+	Permissions []int8
 }
 
 type UserDataset struct {
@@ -47,11 +51,33 @@ func NewUser(userObject UserDataset, factory *PermissionFlyweightFactory) *User 
 	return &user
 }
 
+func (u *User) PermissionsList(p *PermissionFlyweightFactory) []string {
+	permissions := make([]string, len(u.Permissions))
+	for _, permission := range u.Permissions {
+		permissions = append(permissions, p.permissions[permission].Name)
+	}
+	return permissions
+}
+
+type UserNotOptimized struct {
+	Name        string
+	Permissions []*Permission
+}
+
+func NewUserNotUsingFlyweight(userObject UserDataset) *UserNotOptimized {
+	user := UserNotOptimized{Name: userObject.Name}
+	for _, permissionName := range userObject.Permissions {
+		user.Permissions = append(user.Permissions, &Permission{permissionName})
+	}
+	return &user
+}
+
 func main() {
 	userDataset := []UserDataset{
 		{"User1", []string{"can_claim_reward", "can_view_dashboard"}},
 		{"User2", []string{"can_view_dashboard", "can_view_profile"}},
 		{"User3", []string{"can_claim_reward", "can_view_profile"}},
+		{"User4", []string{"can_claim_reward", "can_view_dashboard", "can_view_profile"}},
 	}
 	systemPermissions := []string{"can_claim_reward", "can_view_dashboard", "can_view_profile"}
 	permissionFactory := NewPermissionFlyweightFactory(systemPermissions)
@@ -61,10 +87,28 @@ func main() {
 		users = append(users, NewUser(user, permissionFactory))
 	}
 
-	for _, user := range users {
-		println(user.Name)
-		for _, permission := range user.Permissions {
-			println(permission.Name)
+	fmt.Println("Done")
+	fmt.Println("Total Memory usage, not using flyweight: ")
+	memoryUsage := 0
+	usersNotUsingFlyweight := make([]*UserNotOptimized, 0, len(userDataset))
+	for idx, user := range userDataset {
+		usersNotUsingFlyweight = append(usersNotUsingFlyweight, NewUserNotUsingFlyweight(user))
+		for _, permission := range usersNotUsingFlyweight[idx].Permissions {
+			memoryUsage += len([]byte(permission.Name))
 		}
+		memoryUsage += len(user.Name)
 	}
+	fmt.Printf("Memory usage no Flyweight: %d\n", memoryUsage)
+
+	fmt.Println("Total Memory usage, using flyweight: ")
+	memoryUsage = 0
+	for _, permission := range permissionFactory.permissions {
+		memoryUsage += len([]byte(permission.Name))
+	}
+
+	for _, user := range users {
+		memoryUsage += len(user.Permissions)
+		memoryUsage += len(user.Name)
+	}
+	fmt.Printf("Memory using Flyweight: %d\n", memoryUsage)
 }
